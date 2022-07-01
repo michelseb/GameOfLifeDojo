c67f05585fcf570465f5264952f71b2b47a18e2a
michelseb <sebastien.michel60@gmail.com>
2022-07-01 11:34:11 +0200

    Initial commit

---
+++ GameOfLifeDojo/GameOfLifeDojo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GameOfLifeDojo;
+
+static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Je suis le jeu de la vie :D");
+    }
+}
---
+++ GameOfLifeDojo/GameOfLifeTest/GameCell.Test.cs
@@ -0,0 +1,13 @@
+using GameOfLifeDojo;
+
+namespace GameOfLifeTest;
+
+public class GameCellTest
+{
+    [Fact]
+    public void When_True_Should_Return_Truthy()
+    {
+        Assert.True(true);
+    }
+
+}
